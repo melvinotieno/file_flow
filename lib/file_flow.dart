2cfbb9834ff8a8888933f00d21cd3f59e32cd858
Melvin Otieno <o.melvinotieno@gmail.com>
2024-12-31 12:48:24 +0300

    refactor: Simplify file_flow.dart by removing the FileFlow class and exporting necessary modules

--- lib/file_flow.dart
+++ lib/file_flow.dart
@@ -1,8 +1,2 @@
-
-import 'file_flow_platform_interface.dart';
-
-class FileFlow {
-  Future<String?> getPlatformVersion() {
-    return FileFlowPlatform.instance.getPlatformVersion();
-  }
-}
+export 'src/flow.dart';
+export 'src/models.dart';
