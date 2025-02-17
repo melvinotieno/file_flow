7e199432936f64a7d3e3c800abfa0f870eacc2f2
Melvin Otieno <o.melvinotieno@gmail.com>
2025-02-17 22:05:59 +0300

    feat: enhance urlWithQueryParams function to filter out empty query parameters

--- lib/src/utilities.dart
+++ lib/src/utilities.dart
@@ -1,10 +1,34 @@
+/// Constructs a URL with the given query parameters.
+///
+/// Parameters:
+/// - `url`: The base URL to which the query parameters will be appended.
+/// - `params`: A map of query parameters to append to the URL.
+///
+/// If the `url` already contains query parameters, the given parameters are
+/// appended to the existing ones.
+///
+/// If the `params` map is empty, the `url` is returned as is.
+///
+/// Example:
+/// ```dart
+/// final url = urlWithQueryParams('https://example.com', {
+///  'key1': 'value1',
+///  'key2': 'value2',
+/// });
+///
+/// print(url); // https://example.com?key1=value1&key2=value2
+/// ```
 String urlWithQueryParams(String url, Map<String, String> params) {
   if (params.isEmpty) return url;
 
+  // Determine the separator to use between the URL and the query parameters.
   final separator = url.contains('?') ? '&' : '?';
 
-  final queryParams =
-      params.entries.map((e) => '${e.key}=${e.value}').join('&');
+  // Filter out empty keys and values, and join the entries with '&'.
+  final queryParams = params.entries
+      .where((entry) => entry.key.isNotEmpty && entry.value.isNotEmpty)
+      .map((entry) => '${entry.key}=${entry.value}')
+      .join('&');
 
   return '$url$separator$queryParams';
 }
