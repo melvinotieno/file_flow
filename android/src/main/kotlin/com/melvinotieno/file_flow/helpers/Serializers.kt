9031d34072b0b129241a79e05f41fde8deece112
Melvin Otieno <o.melvinotieno@gmail.com>
2025-02-17 21:15:42 +0300

    feat: update TaskSerializer to use FlowTask

--- android/src/main/kotlin/com/melvinotieno/file_flow/helpers/Serializers.kt
+++ android/src/main/kotlin/com/melvinotieno/file_flow/helpers/Serializers.kt
@@ -1,10 +1,10 @@
 package com.melvinotieno.file_flow.helpers
 
-import com.melvinotieno.file_flow.pigeons.Task
+import com.melvinotieno.file_flow.pigeons.FlowTask
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializer
 
 @OptIn(ExperimentalSerializationApi::class)
-@Serializer(forClass = Task::class)
-object TaskSerializer : KSerializer<Task>
+@Serializer(forClass = FlowTask::class)
+object TaskSerializer : KSerializer<FlowTask>
