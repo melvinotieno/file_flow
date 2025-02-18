fb1a4acb0a0e52d92272a3b93d040e7738985626
Melvin Otieno <o.melvinotieno@gmail.com>
2025-02-18 21:33:23 +0300

    feat: add TaskExceptionSerializer for serializing TaskException

--- android/src/main/kotlin/com/melvinotieno/file_flow/helpers/Serializers.kt
+++ android/src/main/kotlin/com/melvinotieno/file_flow/helpers/Serializers.kt
@@ -1,6 +1,7 @@
 package com.melvinotieno.file_flow.helpers
 
 import com.melvinotieno.file_flow.pigeons.FlowTask
+import com.melvinotieno.file_flow.pigeons.TaskException
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializer
@@ -8,3 +9,7 @@ import kotlinx.serialization.Serializer
 @OptIn(ExperimentalSerializationApi::class)
 @Serializer(forClass = FlowTask::class)
 object TaskSerializer : KSerializer<FlowTask>
+
+@OptIn(ExperimentalSerializationApi::class)
+@Serializer(forClass = TaskException::class)
+object TaskExceptionSerializer : KSerializer<TaskException>
