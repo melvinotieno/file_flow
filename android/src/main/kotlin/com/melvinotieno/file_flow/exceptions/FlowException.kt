60454c3e5f73cbac87b33be8cc21f9f42b3fcafb
Melvin Otieno <o.melvinotieno@gmail.com>
2025-02-19 00:30:45 +0300

    feat: add FlowException class for handling task-related exceptions and remove FlowTaskException

---
+++ android/src/main/kotlin/com/melvinotieno/file_flow/exceptions/FlowException.kt
@@ -0,0 +1,16 @@
+package com.melvinotieno.file_flow.exceptions
+
+import com.melvinotieno.file_flow.pigeons.ErrorCode
+
+/**
+ * Custom exception class to handle task-related exceptions.
+ *
+ * @property code the [ErrorCode] associated with the exception.
+ * @property message the detail message describing the exception.
+ * @property httpResponseCode optional HTTP response code, if any.
+ */
+class FlowException(
+    val code: ErrorCode,
+    override val message: String,
+    val httpResponseCode: String? = null
+) : Exception(message)
