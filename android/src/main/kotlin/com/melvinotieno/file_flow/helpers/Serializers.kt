7eda925b57f50fb7298417c2a20267dc0ce0f69a
Melvin Otieno <o.melvinotieno@gmail.com>
2025-02-19 17:38:26 +0300

    refactor: add serializers for TaskStateData and TaskProgressData

--- android/src/main/kotlin/com/melvinotieno/file_flow/helpers/Serializers.kt
+++ android/src/main/kotlin/com/melvinotieno/file_flow/helpers/Serializers.kt
@@ -2,6 +2,8 @@ package com.melvinotieno.file_flow.helpers
 
 import com.melvinotieno.file_flow.pigeons.FlowTask
 import com.melvinotieno.file_flow.pigeons.TaskException
+import com.melvinotieno.file_flow.pigeons.TaskProgressData
+import com.melvinotieno.file_flow.pigeons.TaskStateData
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializer
@@ -11,5 +13,13 @@ import kotlinx.serialization.Serializer
 object TaskSerializer : KSerializer<FlowTask>
 
 @OptIn(ExperimentalSerializationApi::class)
+@Serializer(forClass = TaskStateData::class)
+object StateDataSerializer : KSerializer<TaskStateData>
+
+@OptIn(ExperimentalSerializationApi::class)
+@Serializer(forClass = TaskProgressData::class)
+object ProgressDataSerializer : KSerializer<TaskProgressData>
+
+@OptIn(ExperimentalSerializationApi::class)
 @Serializer(forClass = TaskException::class)
-object TaskExceptionSerializer : KSerializer<TaskException>
+object ExceptionSerializer : KSerializer<TaskException>
