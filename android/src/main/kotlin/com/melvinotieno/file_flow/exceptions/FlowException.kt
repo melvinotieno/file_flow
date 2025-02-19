ca55e5f92b7730a8326f91e0446a11458d0d7b2f
Melvin Otieno <o.melvinotieno@gmail.com>
2025-02-19 17:10:02 +0300

    refactor: update FlowException to use TaskErrorCode instead of ErrorCode

--- android/src/main/kotlin/com/melvinotieno/file_flow/exceptions/FlowException.kt
+++ android/src/main/kotlin/com/melvinotieno/file_flow/exceptions/FlowException.kt
@@ -1,22 +1,22 @@
 package com.melvinotieno.file_flow.exceptions
 
-import com.melvinotieno.file_flow.pigeons.ErrorCode
+import com.melvinotieno.file_flow.pigeons.TaskErrorCode
 
 /**
  * Exception class for handling task-related errors.
  *
- * @property code the [ErrorCode] associated with the error.
+ * @property code the [TaskErrorCode] associated with the error.
  * @property description the message describing the exception.
  * @property httpResponseCode optional HTTP response code, if any.
  * @property cause the cause of the error, if caused by another exception.
  */
 class FlowException : Exception {
-    val code: ErrorCode
+    val code: TaskErrorCode
     val description: String
     val httpResponseCode: Long?
 
     constructor(
-        code: ErrorCode, description: String, httpResponseCode: Long? = null, cause: Throwable?
+        code: TaskErrorCode, description: String, httpResponseCode: Long? = null, cause: Throwable?
     ) : super(description, cause) {
         this.code = code
         this.description = description
@@ -24,12 +24,12 @@ class FlowException : Exception {
     }
 
     constructor(
-        code: ErrorCode, description: String, httpResponseCode: Long?
+        code: TaskErrorCode, description: String, httpResponseCode: Long?
     ) : this(code, description, httpResponseCode, null)
 
     constructor(
-        code: ErrorCode, description: String, cause: Throwable?
+        code: TaskErrorCode, description: String, cause: Throwable?
     ) : this(code, description, null, cause)
 
-    constructor(code: ErrorCode, description: String) : this(code, description, null, null)
+    constructor(code: TaskErrorCode, description: String) : this(code, description, null, null)
 }
