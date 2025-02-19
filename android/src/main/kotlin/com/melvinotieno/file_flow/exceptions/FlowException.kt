73bd81ffd1bbf06459d197bb74057fec799c1c84
Melvin Otieno <o.melvinotieno@gmail.com>
2025-02-19 15:14:56 +0300

    refactor: enhance FlowException class to support multiple constructors and improve error handling

--- android/src/main/kotlin/com/melvinotieno/file_flow/exceptions/FlowException.kt
+++ android/src/main/kotlin/com/melvinotieno/file_flow/exceptions/FlowException.kt
@@ -3,14 +3,33 @@ package com.melvinotieno.file_flow.exceptions
 import com.melvinotieno.file_flow.pigeons.ErrorCode
 
 /**
- * Custom exception class to handle task-related exceptions.
+ * Exception class for handling task-related errors.
  *
- * @property code the [ErrorCode] associated with the exception.
- * @property message the detail message describing the exception.
+ * @property code the [ErrorCode] associated with the error.
+ * @property description the message describing the exception.
  * @property httpResponseCode optional HTTP response code, if any.
+ * @property cause the cause of the error, if caused by another exception.
  */
-class FlowException(
-    val code: ErrorCode,
-    override val message: String,
-    val httpResponseCode: Long? = null
-) : Exception(message)
+class FlowException : Exception {
+    val code: ErrorCode
+    val description: String
+    val httpResponseCode: Long?
+
+    constructor(
+        code: ErrorCode, description: String, httpResponseCode: Long? = null, cause: Throwable?
+    ) : super(description, cause) {
+        this.code = code
+        this.description = description
+        this.httpResponseCode = httpResponseCode
+    }
+
+    constructor(
+        code: ErrorCode, description: String, httpResponseCode: Long?
+    ) : this(code, description, httpResponseCode, null)
+
+    constructor(
+        code: ErrorCode, description: String, cause: Throwable?
+    ) : this(code, description, null, cause)
+
+    constructor(code: ErrorCode, description: String) : this(code, description, null, null)
+}
