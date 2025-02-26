fad8d61c7b6a70446aa10c4806faea968f3e20a3
Melvin Otieno <o.melvinotieno@gmail.com>
2025-02-26 20:43:02 +0300

    feat: enhance main app to support multiple task examples with a task selector

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:file_flow_example/examples/download_example.dart';
+import 'package:file_flow_example/examples/multi_upload_example.dart';
+import 'package:file_flow_example/examples/parallel_download_example.dart';
+import 'package:file_flow_example/examples/upload_example.dart';
+import 'package:file_flow_example/widgets/task_selector.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,6 +17,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  TaskType _selectedTask = TaskType.download;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,18 +29,45 @@ class _MyAppState extends State<MyApp> {
             IconButton(icon: const Icon(Icons.settings), onPressed: () {}),
           ],
         ),
-        body: const Column(
+        body: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, bottom: 4.0),
+              child: Text(
+                'Task',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ),
+            TaskSelector(
+              selectedTask: _selectedTask,
+              onTaskSelected: (task) => setState(() => _selectedTask = task),
+            ),
             Expanded(
               child: SingleChildScrollView(
-                padding: EdgeInsets.all(16.0),
-                child: DownloadExample(),
+                padding: const EdgeInsets.symmetric(
+                  vertical: 8.0,
+                  horizontal: 16.0,
+                ),
+                child: _buildExample(),
               ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  Widget _buildExample() {
+    switch (_selectedTask) {
+      case TaskType.download:
+        return const DownloadExample();
+      case TaskType.upload:
+        return const UploadExample();
+      case TaskType.parallelDownload:
+        return const ParallelDownloadExample();
+      case TaskType.multiUpload:
+        return const MultiUploadExample();
+    }
   }
 }
