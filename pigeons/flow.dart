6b63156fab3e75914edea0345ebc06b4ecc39610
Melvin Otieno <o.melvinotieno@gmail.com>
2025-02-28 01:25:03 +0300

    feat: add FlowTask, TaskCompleteData, TaskStatus, and TaskProgress classes for task management

--- pigeons/flow.dart
+++ pigeons/flow.dart
@@ -157,3 +157,124 @@ class TaskProgressData {
   /// The network speed in bytes per second.
   final int networkSpeed;
 }
+
+/// The data of a task that has been completed.
+// TODO: Use the proper fields for this class.
+class TaskCompleteData {
+  /// Creates a new instance of [TaskCompleteData].
+  TaskCompleteData({this.path, this.mimeType, this.rawResponse});
+
+  final String? path;
+
+  final String? mimeType;
+
+  final String? rawResponse;
+}
+
+/// The task used by native code for execution.
+class FlowTask {
+  FlowTask({
+    required this.type,
+    required this.id,
+    required this.group,
+    required this.method,
+    required this.headers,
+    required this.timeout,
+    required this.retries,
+    this.proxyAddress,
+    this.proxyPort,
+    this.url,
+    this.urls,
+    this.chunks,
+  });
+
+  /// The type of task to execute.
+  final TaskType type;
+
+  /// The unique identifier of the task.
+  final String id;
+
+  /// The group identifier of the task.
+  final String group;
+
+  /// The HTTP method to use for the request.
+  final String method;
+
+  /// The headers to send with the request.
+  final Map<String, String> headers;
+
+  /// The timeout for the task in milliseconds.
+  final int timeout;
+
+  /// The number of times to retry the task request if it fails.
+  final int retries;
+
+  /// The proxy address to use for the task request.
+  final String? proxyAddress;
+
+  /// The proxy port to use with the proxy address.
+  final int? proxyPort;
+
+  /// The url to use for the task request. This is required for all the tasks
+  /// that are not of type [TaskType.parallelDownload].
+  final String? url;
+
+  /// A list of urls to use for the task request. This is required for tasks of
+  /// type [TaskType.parallelDownload].
+  final List<String>? urls;
+
+  /// The number of chunks to download per URL for [TaskType.parallelDownload].
+  final int? chunks;
+}
+
+/// Represents a task event.
+sealed class TaskEvent {}
+
+/// The status event of a task execution.
+class TaskStatus extends TaskEvent {
+  /// Creates a new instance of [TaskStatus].
+  ///
+  /// If [state] is [TaskState.completed], then [data] is provided.
+  ///
+  /// If [state] is [TaskState.failed], then [exception] is provided.
+  ///
+  /// Otherwise, both [data] and [exception] are `null`.
+  TaskStatus({
+    required this.taskId,
+    required this.state,
+    this.data,
+    this.exception,
+  });
+
+  // TODO: taskId or FlowTask object?
+  final String taskId;
+
+  /// The current state of the task.
+  final TaskState state;
+
+  /// The data associated with a [TaskState.completed] state.
+  final TaskCompleteData? data;
+
+  /// The exception that occurred during the task execution. This is only
+  /// provided if the task state is [TaskState.failed].
+  final TaskException? exception;
+}
+
+/// The progress event of a task execution.
+class TaskProgress extends TaskEvent {
+  /// Creates a new instance of [TaskProgress].
+  TaskProgress({
+    required this.taskId,
+    required this.progress,
+    required this.data,
+  });
+
+  // TODO: taskId or FlowTask object?
+  final String taskId;
+
+  /// The progress of the task.
+  final double progress;
+
+  /// The progress data of the task.
+  final TaskProgressData data;
+}
