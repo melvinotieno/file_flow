6e3a3923443d62a2c3c8dd7833732ff36dfce559
Melvin Otieno <o.melvinotieno@gmail.com>
2025-03-04 02:48:51 +0300

    feat: add utility function to derive download filename from URL and headers

--- android/src/main/kotlin/com/melvinotieno/file_flow/helpers/Utilities.kt
+++ android/src/main/kotlin/com/melvinotieno/file_flow/helpers/Utilities.kt
@@ -1,43 +1,68 @@
 package com.melvinotieno.file_flow.helpers
 
-import android.webkit.MimeTypeMap
+import android.net.Uri
+import kotlin.random.Random
 
 /**
- * Utility class for working with MIME types.
+ * Get the filename for a download task (DownloadTask/ParallelDownloadTask).
+ *
+ * @param url The URL of the download request.
+ * @param headers The response headers of the download request.
+ * @param default The default filename to use if none is found.
+ * @return The filename for the download request.
  */
-class MimeType {
-    companion object {
-        private val mimeTypeMap: MimeTypeMap = MimeTypeMap.getSingleton()
+fun getDownloadFileName(url: String, headers: Map<String, List<String>>, default: String?): String {
+    var filename = default
 
-        /**
-         * Get the file extension from a MIME type.
-         *
-         * @param mimeType The MIME type to get the extension for.
-         * @return The file extension for the MIME type, or null if not found.
-         */
-        fun getExtensionFromMimeType(mimeType: String): String? {
-            return mimeTypeMap.getExtensionFromMimeType(mimeType)
+    if (filename.isNullOrEmpty()) {
+        // Attempt to get filename from response headers
+        (headers["Content-Disposition"] ?: headers["content-disposition"])?.let { disposition ->
+            val filenameHeader = disposition.find {
+                it.contains("filename=") || it.contains("filename*=")
+            }
+
+            filenameHeader?.let { header ->
+                filename = when {
+                    header.contains("filename*=") -> {
+                        val startIndex = header.indexOf("filename*=") + 10
+                        val encodedFilename = header.substring(startIndex).split("''").last()
+                        Uri.decode(encodedFilename)
+                    }
+
+                    header.contains("filename=") -> {
+                        val startIndex = header.indexOf("filename=") + 9
+                        header.substring(startIndex)
+                    }
+
+                    else -> null
+                }
+            }
         }
 
-        /**
-         * Get the MIME type from a file extension.
-         *
-         * @param extension The file extension to get the MIME type for.
-         * @return The MIME type for the file extension, or null if not found.
-         */
-        fun getMimeTypeFromExtension(extension: String): String? {
-            return mimeTypeMap.getMimeTypeFromExtension(extension)
-        }
-
-        /**
-         * Get the MIME type from a file name.
-         *
-         * @param fileName The file name to get the MIME type for.
-         * @return The MIME type for the file name, or "application/octet-stream" if not found.
-         */
-        fun getMimeTypeFromFileName(fileName: String): String {
-            val extension = fileName.substringAfterLast(".", "")
-            return getMimeTypeFromExtension(extension) ?: "application/octet-stream"
+        // If filename is not found in headers, get it from the URL or generate a random one
+        if (filename.isNullOrEmpty()) {
+            try {
+                val uri = Uri.parse(url)
+                filename = uri.lastPathSegment ?: Random.nextInt(Int.MAX_VALUE).toString()
+            } catch (_: Exception) {
+                filename = Random.nextInt(Int.MAX_VALUE).toString()
+            }
         }
     }
+
+    // Check if filename has an extension
+    if (!filename.contains(".")) {
+        // Attempt to get extension from response headers
+        var extension = (headers["Content-Type"] ?: headers["content-type"])?.firstOrNull()?.let {
+            MimeType.getExtensionFromContentType(it)
+        }
+
+        // If extension is not found in headers, get it from the URL
+        if (extension.isNullOrEmpty()) extension = MimeType.getExtensionFromUrl(url)
+
+        // Append extension to filename
+        if (!extension.isNullOrEmpty()) filename += ".$extension"
+    }
+
+    return filename
 }
