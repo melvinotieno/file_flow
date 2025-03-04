461db192ee594f1126a212617cdb962b6486c956
Melvin Otieno <o.melvinotieno@gmail.com>
2025-03-05 01:53:24 +0300

    feat: add function to copy downloaded files to destination directory with support for shared storage

--- android/src/main/kotlin/com/melvinotieno/file_flow/helpers/Utilities.kt
+++ android/src/main/kotlin/com/melvinotieno/file_flow/helpers/Utilities.kt
@@ -1,6 +1,15 @@
 package com.melvinotieno.file_flow.helpers
 
+import android.content.ContentValues
+import android.content.Context
 import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import androidx.core.net.toFile
+import androidx.documentfile.provider.DocumentFile
+import com.melvinotieno.file_flow.pigeons.Task
+import java.io.File
 import kotlin.random.Random
 
 /**
@@ -65,4 +74,76 @@ fun getDownloadFileName(url: String, headers: Map<String, List<String>>, default
     }
 
     return filename
+}
+
+/**
+ * Copy the downloaded file to the destination directory.
+ *
+ * @param context The context to use for copying the file.
+ * @param task The task that the file belongs to.
+ * @param tempFilePath The path of the temporary file.
+ * @param destFileName The name of the destination file.
+ * @return A pair containing the path of the copied file and the MIME type of the file.
+ */
+fun copyDownloadFile(
+    context: Context, task: Task, tempFilePath: String, destFileName: String
+): Pair<String?, String> {
+    val tempFile = File(tempFilePath)
+    val mimeType = MimeType.fromFileName(destFileName)
+
+    val path = task.directoryUri?.let {
+        val directoryUri = Uri.parse(it)
+
+        if (directoryUri.scheme == "file") {
+            val destFile = File(directoryUri.toFile(), destFileName)
+            tempFile.copyTo(destFile, overwrite = true)
+            destFile.absolutePath
+        } else {
+            val documentFile = DocumentFile.fromTreeUri(context, directoryUri)
+            val destFile = documentFile?.createFile(mimeType, destFileName)
+
+            destFile?.uri?.let { uri ->
+                context.contentResolver.openOutputStream(uri)?.use { outputStream ->
+                    tempFile.inputStream().use { inputStream ->
+                        inputStream.copyTo(outputStream)
+                    }
+                }
+                uri.toString()
+            }
+        }
+    } ?: run {
+        if (task.baseDirectory.isSharedStorage) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                val contentValues = ContentValues().apply {
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, destFileName)
+                    put(MediaStore.MediaColumns.MIME_TYPE, mimeType)
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, task.getRelativePath(context))
+                }
+
+                context.contentResolver.let { resolver ->
+                    resolver.insert(task.baseDirectory.mediaStoreUri, contentValues)?.let { uri ->
+                        resolver.openOutputStream(uri)?.use { outputStream ->
+                            tempFile.inputStream().use { inputStream ->
+                                inputStream.copyTo(outputStream)
+                            }
+                        }
+                        uri.toString()
+                    }
+                }
+            } else {
+                val baseDirectoryPath = task.baseDirectory.getPath(context)
+                val destDirParent = Environment.getExternalStoragePublicDirectory(baseDirectoryPath)
+                val destDir = File(destDirParent, task.directory ?: "").also { it.mkdirs() }
+                val destFile = File(destDir, destFileName)
+                tempFile.copyTo(destFile, overwrite = true)
+                destFile.absolutePath
+            }
+        } else {
+            val destFile = File("${task.getRelativePath(context)}/${destFileName}")
+            tempFile.copyTo(destFile, overwrite = true)
+            destFile.absolutePath
+        }
+    }
+
+    return Pair(path, mimeType)
 }
