4fa3a5d7fad03ca03b2621f53d45e3209664b4cc
Melvin Otieno <o.melvinotieno@gmail.com>
2025-03-05 18:51:34 +0300

    refactor: remove unused IsolateHandler and WebHandler classes

--- lib/src/handlers/web_handler.dart
+++
@@ -1,9 +0,0 @@
-import '../models.dart';
-import 'base_handler.dart';
-
-final class WebHandler extends BaseHandler {
-  @override
-  Future<bool> enqueue(FlowTask task) {
-    throw UnimplementedError();
-  }
-}
