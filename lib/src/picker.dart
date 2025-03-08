2a4a0f94c220d9e528bd235f6799223bc1109bde
Melvin Otieno <o.melvinotieno@gmail.com>
2025-03-08 22:38:52 +0300

    feat: enhance Picker functionality with improved permission checks and media selection options

--- lib/src/picker.dart
+++ lib/src/picker.dart
@@ -4,15 +4,19 @@ import 'package:file_flow/src/pigeons/picker.g.dart';
 class Picker {
   static final _api = PickerHostApi();
 
-  /// Checks if the specified URI use permission has been persisted.
+  /// Checks if the specified value has `use permission` persisted.
   ///
   /// Parameters:
-  /// - [uri]: The URI to check.
+  /// - [value]: This can either be a [PickerDirectory] or a [String] that
+  /// represents a directory URI.
   ///
-  /// Returns `true` if the URI use permission has been persisted, `false`
-  /// otherwise.
-  Future<bool> persisted(Uri uri) async {
-    return _api.persisted(uri.toString());
+  /// Returns `true` permission has been persisted, `false` otherwise.
+  Future<bool> persisted(dynamic value) async {
+    if (!(value is String || value is PickerDirectory)) {
+      throw ArgumentError('value must be a String or PickerDirectory');
+    }
+
+    return _api.persisted(value);
   }
 
   /// Allows the user to select a directory from the file system.
@@ -20,13 +24,21 @@ class Picker {
   /// Parameters:
   /// - [directory]: The base directory to start the picker from. This can
   /// either be a [PickerDirectory] or a [String] representing a directory URI.
+  /// - [exact]: Allow the exact directory or any subdirectory to be picked.
+  /// The values allowed are [true], [false] or ['subdirectory'].
   /// - [persist]: Whether the selected directory should be persisted.
   ///
   /// Returns the URI of the selected directory.
   ///
-  /// Throws an [ArgumentError] if the directory provided is neither a String
-  /// nor a PickerDirectory. The directory can be null.
-  Future<Uri> directory({dynamic directory, bool persist = false}) async {
+  /// Throws an [ArgumentError] if the directory provided is not null, a String,
+  /// or a PickerDirectory.
+  ///
+  /// Throws an [ArgumentError] if exact is neither a bool nor 'subdirectory'.
+  Future<Uri> directory({
+    dynamic directory,
+    dynamic exact = false,
+    bool persist = false,
+  }) async {
     if (directory != null &&
         !(directory is String || directory is PickerDirectory)) {
       throw ArgumentError(
@@ -34,7 +46,11 @@ class Picker {
       );
     }
 
-    return Uri.parse(await _api.pickDirectory(directory, persist));
+    if (!(exact is bool || exact == 'subdirectory')) {
+      throw ArgumentError('exact must be a bool or "subdirectory"');
+    }
+
+    return Uri.parse(await _api.pickDirectory(directory, exact, persist));
   }
 
   /// Allows the user to select a file from the file system.
@@ -42,7 +58,8 @@ class Picker {
   /// Parameters:
   /// - [directory]: The base directory to start the picker from. This can
   /// either be a [PickerDirectory] or a [String] representing a directory URI.
-  /// - [extensions]: A list of file extensions to filter the picker by.
+  /// - [mimeTypes]: A list of file extensions to filter the picker by.
+  /// - [exact]: Whether the file must be within the directory given.
   /// - [persist]: Whether the selected file should be persisted.
   ///
   /// Returns the URI of the selected file.
@@ -51,7 +68,8 @@ class Picker {
   /// nor a PickerDirectory. The directory can be null.
   Future<Uri> file({
     dynamic directory,
-    List<String>? extensions,
+    List<String>? mimeTypes,
+    bool exact = false,
     bool persist = false,
   }) async {
     if (directory != null &&
@@ -61,7 +79,7 @@ class Picker {
       );
     }
 
-    return Uri.parse(await _api.pickFile());
+    return Uri.parse(await _api.pickFile(directory, mimeTypes, exact, persist));
   }
 
   /// Allows the user to select multiple files from the file system.
@@ -69,7 +87,8 @@ class Picker {
   /// Parameters:
   /// - [directory]: The base directory to start the picker from. This can
   /// either be a [PickerDirectory] or a [String] representing a directory URI.
-  /// - [extensions]: A list of file extensions to filter the picker by.
+  /// - [mimeTypes]: A list of file extensions to filter the picker by.
+  /// - [exact]: Whether the files must be within the directory given.
   /// - [persist]: Whether the selected files should be persisted.
   ///
   /// Returns the URIs of the selected files.
@@ -78,7 +97,8 @@ class Picker {
   /// nor a PickerDirectory. The directory can be null.
   Future<List<Uri>> files({
     dynamic directory,
-    List<String>? extensions,
+    List<String>? mimeTypes,
+    bool exact = false,
     bool persist = false,
   }) async {
     if (directory != null &&
@@ -88,7 +108,38 @@ class Picker {
       );
     }
 
-    final filePaths = await _api.pickFiles();
-    return filePaths.map((path) => Uri.parse(path)).toList();
+    final paths = await _api.pickFiles(directory, mimeTypes, exact, persist);
+    return paths.map((path) => Uri.parse(path)).toList();
+  }
+
+  /// Allows the user to select a media file (image or video) from the file
+  /// system.
+  ///
+  /// Parameters:
+  /// - [media]: The type of media to pick.
+  /// - [persist]: Whether the selected media file should be persisted.
+  ///
+  /// Returns the URI of the selected media file.
+  Future<Uri> mediaFile({
+    PickerMedia media = PickerMedia.image,
+    bool persist = false,
+  }) async {
+    return Uri.parse(await _api.pickMediaFile(media, persist));
+  }
+
+  /// Allows the user to select multiple media files (images or videos) from the
+  /// file system.
+  ///
+  /// Parameters:
+  /// - [media]: The type of media to pick.
+  /// - [persist]: Whether the selected media files should be persisted.
+  ///
+  /// Returns the URIs of the selected media files.
+  Future<List<Uri>> mediaFiles({
+    PickerMedia media = PickerMedia.image,
+    bool persist = false,
+  }) async {
+    final paths = await _api.pickMediaFiles(media, persist);
+    return paths.map((path) => Uri.parse(path)).toList();
   }
 }
